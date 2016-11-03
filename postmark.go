c92ce82ccbdd6e0709d5cd24fa5cc21e4371ed2c
Anton Novojilov <andy@essentialkaos.com>
2016-11-03 19:26:29 -0400

    Improvements

--- postmark.go
+++ postmark.go
@@ -23,6 +23,7 @@ const (
 	TYPE_POST  uint8 = 0
 	TYPE_PHOTO       = 1
 	TYPE_QUOTE       = 2
+	TYPE_LINK        = 3
 )
 
 // ////////////////////////////////////////////////////////////////////////////////// //
@@ -48,13 +49,23 @@ type Render struct {
 	Bold      func(text string) string
 	Italic    func(text string) string
 	Del       func(text string) string
+	Sup       func(text string) string
+	Sub       func(text string) string
+	Hr        func() string
 	Link      func(url, text string) string
 	Image     func(url, alt, caption string) string
 	Code      func(text string) string
-	CodeBlock func(text, lang string) string
+
+	UnsupportedMacro func(text string) string
 
 	// You can extend formating by custom macroses
-	Macroses map[string]func(args []string) string
+	Macroses []*Macro
+}
+
+type Macro struct {
+	Name      string                                            // Macro name
+	Multiline bool                                              // Mutliline flag
+	Handler   func(data string, props map[string]string) string // Handler function
 }
 
 // ////////////////////////////////////////////////////////////////////////////////// //
@@ -64,19 +75,22 @@ var postTypes = map[string]uint8{
 	"post":  TYPE_POST,
 	"photo": TYPE_PHOTO,
 	"quote": TYPE_QUOTE,
+	"link":  TYPE_LINK,
 }
 
 var (
-	rxMeta         = regexp.MustCompile(`^[+]{4}`)
-	rxFmtHeader    = regexp.MustCompile(`h([1-6])\. (.*)`)
-	rxFmtItalic    = regexp.MustCompile(`_([\p{L}\d\S]{1}.*?)_`)
-	rxFmtBold      = regexp.MustCompile(`\*([\p{L}\d\S]{1}.*?)\*`)
-	rxFmtDel       = regexp.MustCompile(`~([\p{L}\d\S]{1}.*?)~`)
-	rxFmtQuote     = regexp.MustCompile(`^>.*`)
-	rxFmtCode      = regexp.MustCompile("\\`" + `([\p{L}\d\S]{1}.*)` + "\\`")
-	rxFmtImage     = regexp.MustCompile(`\![\w\S]{1,}\.(jpg|jpeg|gif|png)(?:(!|\|)).*`)
-	rxFmtLink      = regexp.MustCompile(`\[(.*?)?(?:\|)?((?:http|https|ftp|mailto)[\S]{3,})\]`)
-	rxFmtCodeBlock = regexp.MustCompile("^[\\`]{3,4}([\\w\\d]{1,})?")
+	rxMeta      = regexp.MustCompile(`^[+]{4}`)
+	rxFmtHeader = regexp.MustCompile(`h([1-6])\. (.*)`)
+	rxFmtItalic = regexp.MustCompile(`_([\p{L}\d\S]{1}.*?)_`)
+	rxFmtBold   = regexp.MustCompile(`\*([\p{L}\d\S]{1}.*?)\*`)
+	rxFmtDel    = regexp.MustCompile(`\-([\p{L}\d\S]{1}.*?)\-`)
+	rxFmtSup    = regexp.MustCompile(`\^([\p{L}\d\S]{1}.*?)\^`)
+	rxFmtSub    = regexp.MustCompile(`\~([\p{L}\d\S]{1}.*?)\~`)
+	rxFmtCode   = regexp.MustCompile("\\`" + `([\p{L}\d\S]{1}.*)` + "\\`")
+	rxFmtHr     = regexp.MustCompile(`^[-]{4,}`)
+	rxFmtImage  = regexp.MustCompile(`\![\w\S]{1,}\.(jpg|jpeg|gif|png)(?:(!|\|)).*`)
+	rxFmtLink   = regexp.MustCompile(`\[(.*?)?(?:\|)?((?:http|https|ftp|mailto)[\S]{3,})\]`)
+	rxMacro     = regexp.MustCompile(`^\{([a-z0-9-]{2,})(?:\:)?(.*)\}`)
 )
 
 // ////////////////////////////////////////////////////////////////////////////////// //
@@ -122,30 +136,60 @@ func processFile(file string, render *Render) (*Post, error) {
 
 	post := &Post{Meta: &PostMeta{}}
 
-	var isMeta bool
-	var isQuote bool
-	var isCode bool
+	var (
+		isMeta     bool
+		isMacro    bool
+		macroName  string
+		macroProps map[string]string
+		macro      *Macro
+		buffer     string
+	)
+
+	var hasMacroses = len(render.Macroses) != 0
 
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		if strings.Trim(line, " ") == "" {
+		if rxMeta.MatchString(line) {
+			isMeta = !isMeta
+			continue
+		}
+
+		if hasMacroses && rxMacro.MatchString(line) {
+			if isMacro {
+				processMutlilineMacro(macro, macroProps, buffer, render, post)
+				isMacro, buffer = false, ""
+				continue
+			}
+
+			macroName, macro, macroProps = parseMacro(line, render)
+
+			if macro == nil {
+				processUnsuportedMacro(macroName, render, post)
+				continue
+			}
+
+			if macro.Multiline {
+				isMacro = true
+				continue
+			}
+
+			processSimpleMacro(macro, macroProps, render, post)
 			continue
 		}
 
 		switch {
-		case rxMeta.MatchString(line):
-			isMeta = !isMeta
-			continue
-		case rxFmtQuote.MatchString(line):
-			isQuote = !isQuote
-		case rxFmtCodeBlock.MatchString(line):
-			isCode = !isCode
-		}
-
-		if isMeta {
+		case isMeta:
 			processMetaData(line, post.Meta)
-		} else {
+
+		case isMacro:
+			buffer += line + "\n"
+
+		default:
+			if strings.Trim(line, " ") == "" {
+				continue
+			}
+
 			processContentData(line, render, post)
 		}
 	}
@@ -199,6 +243,7 @@ func processMetaData(data string, meta *PostMeta) error {
 	return nil
 }
 
+// processContentData process and render post content
 func processContentData(data string, render *Render, post *Post) error {
 	var err error
 
@@ -214,6 +259,7 @@ func processContentData(data string, render *Render, post *Post) error {
 	return err
 }
 
+// processHeaderData process and render header data
 func processHeaderData(data string, render *Render, post *Post) error {
 	if render.Header == nil {
 		appendPostData(post, data)
@@ -230,6 +276,7 @@ func processHeaderData(data string, render *Render, post *Post) error {
 	return nil
 }
 
+// processImageData process and render image data
 func processImageData(data string, render *Render, post *Post) error {
 	if render.Image == nil {
 		appendPostData(post, data)
@@ -246,6 +293,7 @@ func processImageData(data string, render *Render, post *Post) error {
 	return nil
 }
 
+// processParagraphData process and render paragraph data
 func processParagraphData(data string, render *Render, post *Post) error {
 	data = parseParagraph(data, render)
 
@@ -258,6 +306,34 @@ func processParagraphData(data string, render *Render, post *Post) error {
 	return nil
 }
 
+func processSimpleMacro(macro *Macro, macroProps map[string]string, render *Render, post *Post) error {
+	if macro.Handler == nil {
+		return fmt.Errorf("Handler is nil for \"%s\" macro", macro.Name)
+	}
+
+	appendPostData(post, macro.Handler("", macroProps))
+
+	return nil
+}
+
+func processMutlilineMacro(macro *Macro, macroProps map[string]string, data string, render *Render, post *Post) error {
+	if macro.Handler == nil {
+		return fmt.Errorf("Handler is nil for \"%s\" macro", macro.Name)
+	}
+
+	appendPostData(post, macro.Handler(data, macroProps))
+
+	return nil
+}
+
+// processUnsuportedMacro process unsupported macro
+func processUnsuportedMacro(macroName string, render *Render, post *Post) {
+	if render.UnsupportedMacro != nil {
+		appendPostData(post, render.UnsupportedMacro(macroName))
+	}
+}
+
+// parseHeader parse header tag
 func parseHeader(data string) (string, int) {
 	headerInfo := rxFmtHeader.FindStringSubmatch(data)
 	level, err := strconv.Atoi(headerInfo[1])
@@ -269,6 +345,7 @@ func parseHeader(data string) (string, int) {
 	return headerInfo[2], level
 }
 
+// parseImage parse image tag
 func parseImage(data string) (string, string, string) {
 	var url, alt, caption string
 
@@ -285,35 +362,65 @@ func parseImage(data string) (string, string, string) {
 	return url, alt, caption
 }
 
+// parseParagraph parse paragraph data
 func parseParagraph(data string, render *Render) string {
-	if render.Bold != nil && rxFmtBold.MatchString(data) {
-		tags := rxFmtBold.FindAllStringSubmatch(data, -1)
+	var (
+		tags [][]string
+		tag  []string
+	)
 
-		for _, tag := range tags {
+	if render.Bold != nil && rxFmtBold.MatchString(data) {
+		tags = rxFmtBold.FindAllStringSubmatch(data, -1)
+
+		for _, tag = range tags {
 			data = strings.Replace(data, tag[0], render.Bold(tag[1]), -1)
 		}
 	}
 
 	if render.Italic != nil && rxFmtItalic.MatchString(data) {
-		tags := rxFmtItalic.FindAllStringSubmatch(data, -1)
+		tags = rxFmtItalic.FindAllStringSubmatch(data, -1)
 
-		for _, tag := range tags {
+		for _, tag = range tags {
 			data = strings.Replace(data, tag[0], render.Italic(tag[1]), -1)
 		}
 	}
 
 	if render.Del != nil && rxFmtDel.MatchString(data) {
-		tags := rxFmtDel.FindAllStringSubmatch(data, -1)
+		tags = rxFmtDel.FindAllStringSubmatch(data, -1)
 
-		for _, tag := range tags {
+		for _, tag = range tags {
 			data = strings.Replace(data, tag[0], render.Del(tag[1]), -1)
+		}
+	}
+
+	if render.Sup != nil && rxFmtSup.MatchString(data) {
+		tags = rxFmtSup.FindAllStringSubmatch(data, -1)
+
+		for _, tag = range tags {
+			data = strings.Replace(data, tag[0], render.Sup(tag[1]), -1)
+		}
+	}
+
+	if render.Sub != nil && rxFmtSub.MatchString(data) {
+		tags = rxFmtSub.FindAllStringSubmatch(data, -1)
+
+		for _, tag = range tags {
+			data = strings.Replace(data, tag[0], render.Sub(tag[1]), -1)
+		}
+	}
+
+	if render.Hr != nil && rxFmtHr.MatchString(data) {
+		tags = rxFmtHr.FindAllStringSubmatch(data, -1)
+
+		for _, tag = range tags {
+			data = strings.Replace(data, tag[0], render.Hr(), -1)
 		}
 	}
 
 	if render.Code != nil && rxFmtCode.MatchString(data) {
 		tags := rxFmtCode.FindAllStringSubmatch(data, -1)
 
-		for _, tag := range tags {
+		for _, tag = range tags {
 			data = strings.Replace(data, tag[0], render.Code(tag[1]), -1)
 		}
 	}
@@ -329,6 +436,52 @@ func parseParagraph(data string, render *Render) string {
 	return data
 }
 
+// parseMacro extract macro name from given data and return macro struct
+func parseMacro(data string, render *Render) (string, *Macro, map[string]string) {
+	var (
+		macro *Macro
+		name  string
+		props map[string]string
+	)
+
+	macroTag := rxMacro.FindStringSubmatch(data)
+	name = macroTag[1]
+
+	for _, m := range render.Macroses {
+		if name == m.Name {
+			macro = m
+			break
+		}
+	}
+
+	if macroTag[2] != "" {
+		props = parseMacroProps(macroTag[2])
+	}
+
+	return name, macro, props
+}
+
+// parseMacroProps parse macro properties and return it as prop->value map
+func parseMacroProps(data string) map[string]string {
+	if data == "" {
+		return nil
+	}
+
+	result := make(map[string]string)
+
+	for _, prop := range strings.Split(data, "|") {
+		if !strings.Contains(prop, "=") {
+			result[""] = prop
+		} else {
+			propSlice := strings.Split(prop, "=")
+			result[propSlice[0]] = propSlice[1]
+		}
+	}
+
+	return result
+}
+
+// appendPostData append rendered data to post
 func appendPostData(post *Post, data string) {
 	post.Data += data + "\n"
 }
