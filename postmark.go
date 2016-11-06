663cc41603874757b987e62afcc245bbb711b2b0
Anton Novojilov <andy@essentialkaos.com>
2016-11-05 20:06:33 -0400

    Improvements

--- postmark.go
+++ postmark.go
@@ -8,9 +8,9 @@ package postmark
 // ////////////////////////////////////////////////////////////////////////////////// //
 
 import (
-	"bufio"
+	"bytes"
 	"fmt"
-	"os"
+	"io/ioutil"
 	"regexp"
 	"strconv"
 	"strings"
@@ -39,8 +39,8 @@ type PostMeta struct {
 }
 
 type Post struct {
-	Meta *PostMeta // Post meta
-	Data string    // Post rendered data
+	Meta    *PostMeta // Post meta
+	Content string    // Post rendered data
 }
 
 type Render struct {
@@ -97,7 +97,27 @@ var (
 
 // Process parse and render post file
 func Process(file string, render *Render) (*Post, error) {
-	return processFile(file, render)
+	data, err := ioutil.ReadFile(file)
+
+	if err != nil {
+		return nil, err
+	}
+
+	meta, content, err := extractMeta(string(data))
+
+	if err != nil {
+		return nil, err
+	}
+
+	post := &Post{Meta: meta}
+
+	post.Content, err = render.Apply(content)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return post, nil
 }
 
 // ////////////////////////////////////////////////////////////////////////////////// //
@@ -112,103 +132,81 @@ func (p *Post) IsValid() bool {
 		return false
 	}
 
-	if len(p.Data) == 0 {
+	if len(p.Content) == 0 {
 		return false
 	}
 
 	return true
 }
 
-// ////////////////////////////////////////////////////////////////////////////////// //
-
-// processFile parse given file and render data with give render
-func processFile(file string, render *Render) (*Post, error) {
-	fd, err := os.OpenFile(file, os.O_RDONLY, 0)
-
-	if err != nil {
-		return nil, err
-	}
-
-	defer fd.Close()
-
-	reader := bufio.NewReader(fd)
-	scanner := bufio.NewScanner(reader)
-
-	post := &Post{Meta: &PostMeta{}}
-
-	var (
-		isMeta     bool
-		isMacro    bool
-		macroName  string
-		macroProps map[string]string
-		macro      *Macro
-		buffer     string
-	)
-
-	var hasMacroses = len(render.Macroses) != 0
-
-	for scanner.Scan() {
-		line := scanner.Text()
-
-		if rxMeta.MatchString(line) {
-			isMeta = !isMeta
-			continue
-		}
-
-		if hasMacroses && rxMacro.MatchString(line) {
-			if isMacro {
-				processMutlilineMacro(macro, macroProps, buffer, render, post)
-				isMacro, buffer = false, ""
-				continue
-			}
-
-			macroName, macro, macroProps = parseMacro(line, render)
-
-			if macro == nil {
-				processUnsuportedMacro(macroName, render, post)
-				continue
-			}
-
-			if macro.Multiline {
-				isMacro = true
-				continue
-			}
-
-			processSimpleMacro(macro, macroProps, render, post)
-			continue
-		}
-
-		switch {
-		case isMeta:
-			processMetaData(line, post.Meta)
-
-		case isMacro:
-			buffer += line + "\n"
-
-		default:
-			if strings.Trim(line, " ") == "" {
-				continue
-			}
-
-			processContentData(line, render, post)
-		}
-	}
-
-	return post, nil
+// Apply render given data
+func (r *Render) Apply(text string) (string, error) {
+	return parseContent(text, r)
 }
 
-// processMetaLine process line with some metadata info
-func processMetaData(data string, meta *PostMeta) error {
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+// extractMeta extract meta from post content and return meta struct and post content
+// without metadata
+func extractMeta(content string) (*PostMeta, string, error) {
 	var err error
+	var isMeta bool
 
-	metaSlice := strings.Split(data, ":")
+	var meta = &PostMeta{}
 
-	if len(metaSlice) < 2 {
+	var i int
+	var r rune
+
+	var buffer = bytes.NewBuffer(nil)
+
+	for i, r = range content {
+		if r != '\n' {
+			buffer.WriteRune(r)
+			continue
+		}
+
+		if buffer.String() == "++++" {
+			buffer.Reset()
+
+			if isMeta {
+				break
+			}
+
+			isMeta = true
+			continue
+		}
+
+		err = parseMetadataRecord(buffer.String(), meta)
+
+		if err != nil {
+			return nil, "", err
+		}
+
+		buffer.Reset()
+	}
+
+	return meta, content[i:], nil
+}
+
+// parseMetadataRecord process line with some metadata info
+func parseMetadataRecord(data string, meta *PostMeta) error {
+	var (
+		err      error
+		property string
+		value    string
+	)
+
+	delimiter := strings.Index(data, ":")
+
+	if delimiter == -1 || len(data) < delimiter+3 {
 		return fmt.Errorf("Misformatted meta")
 	}
 
-	property := strings.TrimLeft(metaSlice[0], " ")
-	value := strings.TrimLeft(strings.Join(metaSlice[1:], ":"), " ")
+	property, value = data[:delimiter], data[delimiter+2:]
+
+	// Remove spaces
+	property = strings.TrimLeft(property, " ")
+	value = strings.TrimLeft(value, " ")
 
 	switch strings.ToLower(property) {
 	case "title":
@@ -243,99 +241,158 @@ func processMetaData(data string, meta *PostMeta) error {
 	return nil
 }
 
-// processContentData process and render post content
-func processContentData(data string, render *Render, post *Post) error {
+func parseContent(data string, render *Render) (string, error) {
 	var err error
 
-	switch {
-	case rxFmtHeader.MatchString(data):
-		err = processHeaderData(data, render, post)
-	case rxFmtImage.MatchString(data):
-		err = processImageData(data, render, post)
-	default:
-		err = processParagraphData(data, render, post)
+	var dataSize = len(data)
+	var cursor = -1
+
+	var buffer = bytes.NewBuffer(nil)
+	var content string
+	var result string
+
+	var (
+		isMacro    bool
+		macroName  string
+		macroProps map[string]string
+		macro      *Macro
+	)
+
+	var hasMacroses = len(render.Macroses) != 0
+
+	for {
+		cursor++
+
+		if cursor == dataSize {
+			break
+		}
+
+		if data[cursor] != '\n' {
+			buffer.WriteByte(data[cursor])
+			continue
+		}
+
+		// Macro processing
+		if hasMacroses && rxMacro.Match(buffer.Bytes()) {
+			if isMacro {
+				result, err = processMutlilineMacro(macro, macroProps, buffer, render)
+
+				if err != nil {
+					return "", err
+				}
+
+				content += result + "\n"
+				isMacro, macro, macroName, macroProps = false, nil, "", nil
+				buffer.Reset()
+				continue
+			}
+
+			macroName, macro, macroProps = parseMacro(buffer, render)
+
+			if macro == nil {
+				result, err = processUnsuportedMacro(macroName, render)
+
+				if err != nil {
+					return "", err
+				}
+
+				content += result + "\n"
+				buffer.Reset()
+				continue
+			}
+
+			if macro.Multiline {
+				isMacro = true
+				buffer.Reset()
+				continue
+			}
+
+			result, err = processSimpleMacro(macro, macroProps, render)
+
+			if err != nil {
+				return "", err
+			}
+
+			content += result + "\n"
+			buffer.Reset()
+			continue
+		}
+
+		if len(bytes.Trim(buffer.Bytes(), " ")) == 0 {
+			buffer.Reset()
+			continue
+		}
+
+		result, err = processContentData(buffer, render)
+
+		if err != nil {
+			return "", err
+		}
+
+		content += result + "\n"
+
+		buffer.Reset()
 	}
 
-	return err
+	return content, nil
+}
+
+// processContentData process and render post content
+func processContentData(data *bytes.Buffer, render *Render) (string, error) {
+	switch {
+	case rxFmtHeader.Match(data.Bytes()):
+		return processHeaderData(data, render)
+	case rxFmtImage.Match(data.Bytes()):
+		return processImageData(data, render)
+	}
+
+	return processParagraphData(data, render)
 }
 
 // processHeaderData process and render header data
-func processHeaderData(data string, render *Render, post *Post) error {
+func processHeaderData(data *bytes.Buffer, render *Render) (string, error) {
 	if render.Header == nil {
-		appendPostData(post, data)
+		return data.String(), nil
 	}
 
 	text, level := parseHeader(data)
 
 	if level == -1 {
-		return fmt.Errorf("Can't parse header line \"%s\"", data)
+		return "", fmt.Errorf("Can't parse header line \"%s\"", data)
 	}
 
-	appendPostData(post, render.Header(text, level))
-
-	return nil
+	return render.Header(text, level), nil
 }
 
 // processImageData process and render image data
-func processImageData(data string, render *Render, post *Post) error {
+func processImageData(data *bytes.Buffer, render *Render) (string, error) {
 	if render.Image == nil {
-		appendPostData(post, data)
+		return data.String(), nil
 	}
 
 	url, alt, caption := parseImage(data)
 
 	if caption != "" {
-		caption = parseParagraph(caption, render)
+		caption = parseParagraph(bytes.NewBufferString(caption), render)
 	}
 
-	appendPostData(post, render.Image(url, alt, caption))
-
-	return nil
+	return render.Image(url, alt, caption), nil
 }
 
 // processParagraphData process and render paragraph data
-func processParagraphData(data string, render *Render, post *Post) error {
-	data = parseParagraph(data, render)
+func processParagraphData(data *bytes.Buffer, render *Render) (string, error) {
+	result := parseParagraph(data, render)
 
 	if render.Paragraph == nil {
-		appendPostData(post, data)
+		return result, nil
 	}
 
-	appendPostData(post, render.Paragraph(data))
-
-	return nil
-}
-
-func processSimpleMacro(macro *Macro, macroProps map[string]string, render *Render, post *Post) error {
-	if macro.Handler == nil {
-		return fmt.Errorf("Handler is nil for \"%s\" macro", macro.Name)
-	}
-
-	appendPostData(post, macro.Handler("", macroProps))
-
-	return nil
-}
-
-func processMutlilineMacro(macro *Macro, macroProps map[string]string, data string, render *Render, post *Post) error {
-	if macro.Handler == nil {
-		return fmt.Errorf("Handler is nil for \"%s\" macro", macro.Name)
-	}
-
-	appendPostData(post, macro.Handler(data, macroProps))
-
-	return nil
-}
-
-// processUnsuportedMacro process unsupported macro
-func processUnsuportedMacro(macroName string, render *Render, post *Post) {
-	if render.UnsupportedMacro != nil {
-		appendPostData(post, render.UnsupportedMacro(macroName))
-	}
+	return render.Paragraph(result), nil
 }
 
 // parseHeader parse header tag
-func parseHeader(data string) (string, int) {
-	headerInfo := rxFmtHeader.FindStringSubmatch(data)
+func parseHeader(data *bytes.Buffer) (string, int) {
+	headerInfo := rxFmtHeader.FindStringSubmatch(data.String())
 	level, err := strconv.Atoi(headerInfo[1])
 
 	if err != nil {
@@ -346,10 +403,10 @@ func parseHeader(data string) (string, int) {
 }
 
 // parseImage parse image tag
-func parseImage(data string) (string, string, string) {
+func parseImage(data *bytes.Buffer) (string, string, string) {
 	var url, alt, caption string
 
-	imageTag := rxFmtImage.FindAllString(data, -1)
+	imageTag := rxFmtImage.FindAllString(data.String(), -1)
 	imageTagSlice := strings.Split(imageTag[0], "!")
 
 	url, caption = imageTagSlice[1], strings.TrimLeft(imageTagSlice[2], " ")
@@ -363,89 +420,91 @@ func parseImage(data string) (string, string, string) {
 }
 
 // parseParagraph parse paragraph data
-func parseParagraph(data string, render *Render) string {
+func parseParagraph(data *bytes.Buffer, render *Render) string {
 	var (
-		tags [][]string
-		tag  []string
+		tags [][][]byte
+		tag  [][]byte
 	)
 
-	if render.Bold != nil && rxFmtBold.MatchString(data) {
-		tags = rxFmtBold.FindAllStringSubmatch(data, -1)
+	var dataBytes = data.Bytes()
+
+	if render.Bold != nil && rxFmtBold.Match(dataBytes) {
+		tags = rxFmtBold.FindAllSubmatch(dataBytes, -1)
 
 		for _, tag = range tags {
-			data = strings.Replace(data, tag[0], render.Bold(tag[1]), -1)
+			dataBytes = bytes.Replace(dataBytes, tag[0], []byte(render.Bold(string(tag[1]))), -1)
 		}
 	}
 
-	if render.Italic != nil && rxFmtItalic.MatchString(data) {
-		tags = rxFmtItalic.FindAllStringSubmatch(data, -1)
+	if render.Italic != nil && rxFmtItalic.Match(dataBytes) {
+		tags = rxFmtItalic.FindAllSubmatch(dataBytes, -1)
 
 		for _, tag = range tags {
-			data = strings.Replace(data, tag[0], render.Italic(tag[1]), -1)
+			dataBytes = bytes.Replace(dataBytes, tag[0], []byte(render.Italic(string(tag[1]))), -1)
 		}
 	}
 
-	if render.Del != nil && rxFmtDel.MatchString(data) {
-		tags = rxFmtDel.FindAllStringSubmatch(data, -1)
+	if render.Del != nil && rxFmtDel.Match(dataBytes) {
+		tags = rxFmtDel.FindAllSubmatch(dataBytes, -1)
 
 		for _, tag = range tags {
-			data = strings.Replace(data, tag[0], render.Del(tag[1]), -1)
+			dataBytes = bytes.Replace(dataBytes, tag[0], []byte(render.Del(string(tag[1]))), -1)
 		}
 	}
 
-	if render.Sup != nil && rxFmtSup.MatchString(data) {
-		tags = rxFmtSup.FindAllStringSubmatch(data, -1)
+	if render.Sup != nil && rxFmtSup.Match(dataBytes) {
+		tags = rxFmtSup.FindAllSubmatch(dataBytes, -1)
 
 		for _, tag = range tags {
-			data = strings.Replace(data, tag[0], render.Sup(tag[1]), -1)
+			dataBytes = bytes.Replace(dataBytes, tag[0], []byte(render.Sup(string(tag[1]))), -1)
 		}
 	}
 
-	if render.Sub != nil && rxFmtSub.MatchString(data) {
-		tags = rxFmtSub.FindAllStringSubmatch(data, -1)
+	if render.Sub != nil && rxFmtSub.Match(dataBytes) {
+		tags = rxFmtSub.FindAllSubmatch(dataBytes, -1)
 
 		for _, tag = range tags {
-			data = strings.Replace(data, tag[0], render.Sub(tag[1]), -1)
+			dataBytes = bytes.Replace(dataBytes, tag[0], []byte(render.Sub(string(tag[1]))), -1)
 		}
 	}
 
-	if render.Hr != nil && rxFmtHr.MatchString(data) {
-		tags = rxFmtHr.FindAllStringSubmatch(data, -1)
+	if render.Hr != nil && rxFmtHr.Match(dataBytes) {
+		tags = rxFmtHr.FindAllSubmatch(dataBytes, -1)
 
 		for _, tag = range tags {
-			data = strings.Replace(data, tag[0], render.Hr(), -1)
+			dataBytes = bytes.Replace(dataBytes, tag[0], []byte(render.Hr()), -1)
 		}
 	}
 
-	if render.Code != nil && rxFmtCode.MatchString(data) {
-		tags := rxFmtCode.FindAllStringSubmatch(data, -1)
+	if render.Code != nil && rxFmtCode.Match(dataBytes) {
+		tags := rxFmtCode.FindAllSubmatch(dataBytes, -1)
 
 		for _, tag = range tags {
-			data = strings.Replace(data, tag[0], render.Code(tag[1]), -1)
+			dataBytes = bytes.Replace(dataBytes, tag[0], []byte(render.Code(string(tag[1]))), -1)
 		}
 	}
 
-	if render.Link != nil && rxFmtLink.MatchString(data) {
-		tags := rxFmtLink.FindAllStringSubmatch(data, -1)
+	if render.Link != nil && rxFmtLink.Match(dataBytes) {
+		tags := rxFmtLink.FindAllSubmatch(dataBytes, -1)
 
 		for _, tag := range tags {
-			data = strings.Replace(data, tag[0], render.Link(tag[2], tag[1]), -1)
+			dataBytes = bytes.Replace(dataBytes, tag[0], []byte(render.Link(string(tag[2]), string(tag[1]))), -1)
 		}
 	}
 
-	return data
+	return string(dataBytes)
 }
 
 // parseMacro extract macro name from given data and return macro struct
-func parseMacro(data string, render *Render) (string, *Macro, map[string]string) {
+func parseMacro(data *bytes.Buffer, render *Render) (string, *Macro, map[string]string) {
 	var (
 		macro *Macro
 		name  string
 		props map[string]string
 	)
 
-	macroTag := rxMacro.FindStringSubmatch(data)
-	name = macroTag[1]
+	macroTag := rxMacro.FindSubmatch(data.Bytes())
+	name = string(macroTag[1])
 
 	for _, m := range render.Macroses {
 		if name == m.Name {
@@ -454,8 +513,8 @@ func parseMacro(data string, render *Render) (string, *Macro, map[string]string)
 		}
 	}
 
-	if macroTag[2] != "" {
-		props = parseMacroProps(macroTag[2])
+	if len(macroTag[2]) != 0 {
+		props = parseMacroProps(string(macroTag[2]))
 	}
 
 	return name, macro, props
@@ -481,7 +540,27 @@ func parseMacroProps(data string) map[string]string {
 	return result
 }
 
-// appendPostData append rendered data to post
-func appendPostData(post *Post, data string) {
-	post.Data += data + "\n"
+func processSimpleMacro(macro *Macro, macroProps map[string]string, render *Render) (string, error) {
+	if macro.Handler == nil {
+		return "", fmt.Errorf("Handler is nil for \"%s\" macro", macro.Name)
+	}
+
+	return macro.Handler("", macroProps), nil
+}
+
+func processMutlilineMacro(macro *Macro, macroProps map[string]string, data *bytes.Buffer, render *Render) (string, error) {
+	if macro.Handler == nil {
+		return "", fmt.Errorf("Handler is nil for \"%s\" macro", macro.Name)
+	}
+
+	return macro.Handler(data.String(), macroProps), nil
+}
+
+// processUnsuportedMacro process unsupported macro
+func processUnsuportedMacro(macroName string, render *Render) (string, error) {
+	if render.UnsupportedMacro != nil {
+		return render.UnsupportedMacro(macroName), nil
+	}
+
+	return "", nil
 }
